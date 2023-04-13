8316368e5add0ca6283b994e175cbdce6488c418
milaShurupova <mila.shurupova@gmail.com>
2023-04-13 14:07:56 +0300

    scrapper to wiki

---
+++ app.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+
+
+const url = "https://en.wikipedia.org/wiki/Cosmonautics_Day";
+
+
+async function scrapeData() {
+    try {
+
+        const { data } = await axios.get(url);
+        const $ = cheerio.load(data);
+
+        const listItems = $(".mw-parser-output ul li");
+        console.log(listItems.html());
+
+        listItems.each(function (idx, el) {
+            console.log($(el).text());
+        });
+
+        const names = [];
+        listItems.each((idx, el) => {
+
+            const name = { name: "", num: "" };
+            name.name = $(el).children("a").text();
+            names.push(name);
+        });
+
+        console.dir(names);
+
+    } catch (err) {
+        console.error(err);
+    }
+    
+}
+
+scrapeData();
+
+
+
+
